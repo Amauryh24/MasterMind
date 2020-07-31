261cc96023b8bb48b97b89e909e2926df76aeb28
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-08-01 00:09:00 +0200

    function insertColor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,64 @@
 masterMind = {
-  colors: {
-    1: "black",
-    2: "white",
-    3: "red",
-    4: "orange",
-    5: "yellow",
-    6: "blue",
-  },
+  colors: ["black", "white", "red", "orange", "yellow", "blue"],
   settings: {
     lines: 12,
-    column: 4,
+    columns: 4,
   },
   game: {
-    colors: 6,
+    line: 1,
+    codeSoluce: [],
+    codeEssais: [],
+  },
+  elementsDom: {
+    codeSoluce: document.getElementById("codeSoluce"),
+
+    colorOptions: document.getElementById("colorOptions"),
   },
 
   initialise: function () {
-    console.log("ok");
     this.drawGameBoard();
+  },
+
+  drawGameBoard: function () {
+    let board = document.getElementById("board");
+    board.innerHTML = "";
+    // display boardGame spot & indicators
+    for (let i = 0; i < this.settings.lines; i++) {
+      let line = document.createElement("li");
+      line.setAttribute("class", "line");
+
+      board.appendChild(line);
+
+      lineId = document.createElement("span");
+      lineId.innerText = i + 1;
+      line.appendChild(lineId);
+
+      for (let j = 0; j < this.settings.columns; j++) {
+        let spot = document.createElement("span");
+        spot.setAttribute("class", "spot");
+        line.appendChild(spot);
+      }
+      for (let k = 0; k < this.settings.columns; k++) {
+        let indicator = document.createElement("span");
+        indicator.setAttribute("class", "indicator");
+        line.appendChild(indicator);
+      }
+    }
+    // display list colors and add listener for each color
+    colorOptions = document.getElementById("colorOptions");
+    for (let i = 0; i < masterMind.colors.length; i++) {
+      let color = document.createElement("li");
+      color.setAttribute("class", "option " + this.colors[i]);
+      colorOptions.appendChild(color);
+
+      color.addEventListener("click", this.insertColor);
+    }
+  },
+  insertColor: function (e) {
+    let currentLine = document.getElementsByClassName("line")[
+      masterMind.game.line - 1
+    ];
+    let spots = currentLine.getElementsByClassName("spot");
+    spots[0].className = "black";
   },
 };
