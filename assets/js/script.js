042065d779eaaf1f362fdded14017997324f98de
Amaury Henrottay <amaury.henrottay@gmail.com>
2020-08-01 05:58:30 +0200

    insertIndicator, gameSatus, codeRevelatino

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,7 @@ let masterMind = {
   initialise: function () {
     this.codeSoluce();
     this.drawGameBoard();
+    this.codeRevelation();
   },
   codeSoluce: function () {
     for (let i = 0; i < this.settings.columns; i++) {
@@ -31,7 +32,7 @@ let masterMind = {
     for (let i = 0; i < this.settings.columns; i++) {
       let codeSoluce = document.createElement("li");
       codeSoluce.innerText = "?";
-      codeSoluce.setAttribute("class", this.game.codeSoluce[i]); // don't forget to cut it !
+      codeSoluce.setAttribute("class", "soluce");
       this.elementsDom.codeSoluce.appendChild(codeSoluce);
     }
 
@@ -76,14 +77,71 @@ let masterMind = {
     let spots = currentLine.getElementsByClassName("spot");
     spots[0].className = color.target.id;
     masterMind.game.codeEssais.push(color.target.id);
-    console.log(masterMind.game.codeEssais);
+
+    // check code when line is completed
     if (masterMind.game.codeEssais.length === masterMind.settings.columns) {
-      masterMind.codeCheck();
-      masterMind.game.line++;
-      masterMind.game.codeEssais = [];
+      if (masterMind.codeCheck()) masterMind.gameStatus("win");
+      else masterMind.game.line++;
+    }
+
+    if (masterMind.game.line === masterMind.settings.lines + 1) {
+      masterMind.gameStatus("lose");
     }
   },
   codeCheck: function () {
-    console.log("je vérifie le code");
+    codeMatch = true;
+    let codeSoluceCopy = this.game.codeSoluce.slice(0);
+    // check good color in good place
+    for (let i = 0; i < this.settings.columns; i++) {
+      if (this.game.codeSoluce[i] === this.game.codeEssais[i]) {
+        this.game.codeEssais[i] = "hit";
+        this.insertIndicators("hit");
+        codeSoluceCopy[i] = -1;
+      } else {
+        codeMatch = false;
+      }
+    }
+    // check good color but no good place
+    for (let i = 0; i < this.settings.columns; i++) {
+      if (codeSoluceCopy.indexOf(this.game.codeEssais[i]) !== -1)
+        this.insertIndicators("almost");
+    }
+    masterMind.game.codeEssais = [];
+
+    return codeMatch;
+  },
+  insertIndicators: function (status) {
+    let currentLine = document.getElementsByClassName("line")[
+      masterMind.game.line - 1
+    ];
+    let indicators = currentLine.getElementsByClassName("indicator");
+    indicators[0].className = status;
+  },
+
+  gameStatus: function (status) {
+    this.codeRevelation();
+    let modal = document.getElementById("modal");
+    let message = document.getElementById("modalMessage");
+    modal.style.display = "flex";
+    if (status === "win") {
+      message.innerText = "You craked the Code !";
+    } else {
+      message.innerText = "You lose";
+    }
+    document.getElementById("yes").addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+
+    document.getElementById("no").addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+  },
+  codeRevelation: function () {
+    let codeSoluce = this.elementsDom.codeSoluce.getElementsByClassName(
+      "soluce"
+    );
+    for (let i = 0; i < this.settings.columns; i++) {
+      codeSoluce[0].setAttribute("class", this.game.codeSoluce[i]);
+    }
   },
 };
